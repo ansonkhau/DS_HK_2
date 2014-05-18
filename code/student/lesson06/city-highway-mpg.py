1c2f1dbc0a85444c9fba148c124d684b5e59d7eb
Anson <ansonkhau@gmail.com>
2014-05-19 06:56:22 +0800

    Update

--- code/student/lesson06/city-highway-mpg.py
+++ code/student/lesson06/city-highway-mpg.py
@@ -1,15 +1,30 @@
-import pandas as pd 
-import numpy as np 
+#import pandas as pd 
+#from pandas import read_csv, DataFrame
+#import numpy as np 
+#from sklearn import linear_model, feature_selection
+#from sklearn.feature_selection.univariate_selection import f_regression
+
+from pandas import read_csv, DataFrame
+from numpy import mean
 from sklearn import linear_model, feature_selection
 from sklearn.feature_selection.univariate_selection import f_regression
 
 # Store data in a consistent place
 DATA_DIR = '/users/ansonau/DS_HK_2/data/'
 
-cars = pd.read_csv(DATA_DIR + 'cars93.csv')
+cars = read_csv(DATA_DIR + 'cars93.csv')
 
-X = cars[['EngineSize','Horsepower','RPM','Rev.per.mile','Fuel.tank.capacity','Passengers','Length','Wheelbase','Width','Turn.circle','Weight']]
-f,p = f_regression(X, cars['MPG.city'].values)  
+
+#Clean Data
+#Get only Numeric Data from cars
+car_x = cars._get_numeric_data()
+car_x = car_x.fillna(car_x.mean())
+#Drop y Values
+#car_x = cars.drop(['MPG.highway','MPG.city','Manufacturer','Model','Type','AirBags','DriveTrain','Origin','Make'],1)
+car_x = cars[['EngineSize','Horsepower','RPM','Rev.per.mile','Fuel.tank.capacity','Passengers','Length','Wheelbase','Width','Turn.circle','Weight']]
+car_y = cars['MPG.highway']
+
+f,p = f_regression(car_x, car_y.values)  
 print [ [x, y] for x,y in zip(f, p)]
 
 
@@ -18,17 +33,20 @@ print [ [x, y] for x,y in zip(f, p)]
 #Polynomial Regression 
 cars['Weight_squared'] = cars['Weight']**2
 cars['Weight_cubed'] = cars['Weight'] **3
+cars['Weigh_fourth'] = cars['Weight'] **4
 MPG = cars['MPG.city']
-Weight_poly = [ [x, y, z] for x,y,z in zip(cars['Weight'].values, cars['Weight_squared'].values,cars['Weight_cubed'])]
+Weight_poly = [ [w, x, y, z] for w, x,y,z in zip(cars['Weight'].values, cars['Weight_squared'].values,cars['Weight_cubed'],cars['Weigh_fourth'])]
 
 ridge = linear_model.Ridge()
 ridge.fit(Weight_poly,MPG)
+#s = 'The value of x is ' + repr(x) + ', and y is ' + repr(y) + '...'
+print 'Coef 1: ' + str(ridge.coef_[3])
+print 'Coef 2: ' + str(ridge.coef_[2])
+print 'Coef 3: ' + str(ridge.coef_[1])
+print 'Coef 0: ' + str(ridge.coef_[0])
 
-print ridge.coef_[2]
-print ridge.coef_[1]
-print ridge.coef_[0]
 
 
-response = ((ridge.coef_[2] * cars['Weight'])**3)+((ridge.coef_[1] * cars['Weight'])**2) + ((ridge.coef_[0] * cars['Weight'])) + ridge.intercept_
+response = ((ridge.coef_[2] * cars['Weight'])**4)+((ridge.coef_[2] * cars['Weight'])**3)+((ridge.coef_[1] * cars['Weight'])**2) + ((ridge.coef_[0] * cars['Weight'])) + ridge.intercept_
 
-print ridge.score(cars[['Weight','Weight_squared','Weight_cubed']].values, MPG)
+print 'Ridge Score: ' + str(ridge.score(cars[['Weight','Weight_squared','Weight_cubed','Weigh_fourth']].values, MPG))
