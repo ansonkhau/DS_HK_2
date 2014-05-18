930dfb0262dba602051fa2c6c6e5f6c9e6090df4
Anson <ansonkhau@gmail.com>
2014-05-18 21:11:18 +0800

    Polynomial regression City-Highway.mpg

--- code/student/lesson06/city-highway-mpg.py
+++ code/student/lesson06/city-highway-mpg.py
@@ -8,9 +8,27 @@ DATA_DIR = '/users/ansonau/DS_HK_2/data/'
 
 cars = pd.read_csv(DATA_DIR + 'cars93.csv')
 
-X = cars[['EngineSize','Horsepower','RPM','Rev.per.mile','Fuel.tank.capacity','Passengers','Length','Wheelbase','Width','Turn.circle']]
-f,p = f_regression(X, cars['MPG.highway'].values)  
+X = cars[['EngineSize','Horsepower','RPM','Rev.per.mile','Fuel.tank.capacity','Passengers','Length','Wheelbase','Width','Turn.circle','Weight']]
+f,p = f_regression(X, cars['MPG.city'].values)  
 print [ [x, y] for x,y in zip(f, p)]
 
 
-#,'Rear.seat.room','Luggage.room','Weight'
+# Regression on Weight
+
+#Polynomial Regression 
+cars['Weight_squared'] = cars['Weight']**2
+cars['Weight_cubed'] = cars['Weight'] **3
+MPG = cars['MPG.city']
+Weight_poly = [ [x, y, z] for x,y,z in zip(cars['Weight'].values, cars['Weight_squared'].values,cars['Weight_cubed'])]
+
+ridge = linear_model.Ridge()
+ridge.fit(Weight_poly,MPG)
+
+print ridge.coef_[2]
+print ridge.coef_[1]
+print ridge.coef_[0]
+
+
+response = ((ridge.coef_[2] * cars['Weight'])**3)+((ridge.coef_[1] * cars['Weight'])**2) + ((ridge.coef_[0] * cars['Weight'])) + ridge.intercept_
+
+print ridge.score(cars[['Weight','Weight_squared','Weight_cubed']].values, MPG)
